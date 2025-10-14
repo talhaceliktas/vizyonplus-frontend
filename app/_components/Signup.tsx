99405e52735ff4a4e6a9209b7c4df220654e994f
talhaceliktas <avrol928@gmail.com>
2025-10-14 22:08:10 +0300

    login ekraninin genel tasarimi tamamlandi

--- app/_components/Signup.tsx
+++ app/_components/Signup.tsx
@@ -1,14 +1,50 @@
+import Link from "next/link";
 import { openSans } from "../_lib/fonts";
+import { FaLock, FaUser } from "react-icons/fa";
 
 const Signup = () => {
   return (
     <div
-      className={`kutu text-primary-900 flex h-full w-full flex-col items-center gap-y-3 p-5 ${openSans.className}`}
+      className={`flex h-full w-full flex-col items-center justify-center gap-y-3 bg-cover bg-center p-5 ${openSans.className} `}
+      style={{ backgroundImage: "url('/loginBG.webp')" }}
     >
-      <h3 className="text-3xl font-semibold">Giriş Yap</h3>
-      <p className="text-gray-200">
-        Hoşgeldiniz! Lütfen gerekli alanları doldurunuz.
-      </p>
+      <div className="border-primary-200 flex flex-col items-center gap-y-6 border-[1px] p-8 shadow-2xl backdrop-blur-sm">
+        <h3 className="text-3xl font-semibold">Giriş Yap</h3>
+        <p className="text-primary-50 opacity-75">
+          Hoşgeldiniz! Lütfen gerekli alanları doldurunuz.
+        </p>
+        <div className="mt-10 mb-8 flex w-full flex-col gap-5">
+          <div className="relative w-full">
+            <input
+              type="text"
+              placeholder="E-mail"
+              className="placeholder:text-primary-50 border-primary-100 w-full rounded-full border-[2px] py-2 pl-4"
+            />
+            <FaUser className="absolute top-1/2 right-5 -translate-y-1/2 text-xl" />
+          </div>
+          <div className="relative w-full">
+            <input
+              type="text"
+              placeholder="Şifre"
+              className="placeholder:text-primary-50 border-primary-100 w-full rounded-full border-[2px] py-2 pl-4"
+            />
+            <FaLock className="absolute top-1/2 right-5 -translate-y-1/2 text-xl" />
+          </div>
+        </div>
+        <div className="flex w-full justify-between">
+          <div className="flex gap-x-2">
+            <input type="checkbox" className="block" id="beniHatirla" />
+            <label htmlFor="beniHatirla">Beni Hatırla</label>
+          </div>
+          <Link href={""} className="block">
+            Şifremi Unuttum
+          </Link>
+        </div>
+        <button className="bg-primary-300/30 w-full rounded-full py-2">
+          Giriş Yap
+        </button>
+        <button>Hemen Kayıt Ol!</button>
+      </div>
     </div>
   );
 };
--- app/giris/page.tsx
+++ app/giris/page.tsx
@@ -2,8 +2,8 @@ import Signup from "../_components/Signup";
 
 const Page = () => {
   return (
-    <div className="h-screen w-full px-4 pt-44 pb-22">
-      <div className="dis-kutu mx-auto h-full w-full max-w-[1360px] rounded-2xl bg-gray-50">
+    <div className="h-screen w-full">
+      <div className="dis-kutu h-full w-full rounded-2xl bg-gray-50">
         <Signup />
       </div>
     </div>
