db29d3078e81e82601c50015cb1ad53566ee977c
talhaceliktas <avrol928@gmail.com>
2025-10-29 20:52:21 +0300

    yukleniyor spinnerlari eklendi

--- app/profil/dahaSonraIzle/page.tsx
+++ app/profil/dahaSonraIzle/page.tsx
@@ -1,12 +1,16 @@
+import { Suspense } from "react";
 import Izlenecekler from "../../_components/profil/Izlenecekler";
 import ProfilYanMenu from "../../_components/profil/ProfilYanMenu";
+import Yukleniyor from "../../_components/icerikler/Yukleniyor";
 
 const Page = () => {
   return (
     <div className="flex w-full gap-x-10">
       <ProfilYanMenu routeHref="/profil/dahaSonraIzle" />
       <div className="flex-1">
-        <Izlenecekler />
+        <Suspense fallback={<Yukleniyor />}>
+          <Izlenecekler />
+        </Suspense>
       </div>
     </div>
   );
--- app/profil/favoriler/page.tsx
+++ app/profil/favoriler/page.tsx
@@ -1,12 +1,16 @@
+import { Suspense } from "react";
 import Favoriler from "../../_components/profil/Favoriler";
 import ProfilYanMenu from "../../_components/profil/ProfilYanMenu";
+import Yukleniyor from "../../_components/icerikler/Yukleniyor";
 
 const Page = () => {
   return (
     <div className="flex w-full gap-x-10">
       <ProfilYanMenu routeHref="/profil/favoriler" />
       <div className="flex-1">
-        <Favoriler />
+        <Suspense fallback={<Yukleniyor />}>
+          <Favoriler />
+        </Suspense>
       </div>
     </div>
   );
