b42faca6f04b1aaab8bbb6054d4b21011d52c95c
talhaceliktas <avrol928@gmail.com>
2025-11-01 18:58:12 +0300

    loading kismi guncellendi

--- app/loading.tsx
+++ app/loading.tsx
@@ -6,7 +6,7 @@ const Loading = () => {
   return (
     <div className="fixed flex h-screen w-screen items-center justify-center">
       <div className="flex w-32 flex-col items-center gap-y-6">
-        <div className="relative aspect-[4/1] w-40 md:w-60">
+        <div className="relative aspect-[803/310] w-20 md:w-40">
           <Image
             src={vizyonPlusLogo}
             alt="Vizyon PlusLogo"
@@ -14,7 +14,7 @@ const Loading = () => {
             sizes="10rem, 15rem"
           />
         </div>
-        <SyncLoader size={30} color="var(--color-secondary-2)" />
+        <SyncLoader size={22} color="var(--color-secondary-2)" />
       </div>
     </div>
   );
